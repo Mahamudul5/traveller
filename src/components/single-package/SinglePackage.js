df82814bbe5492781e932bf30c92c0a93e2e9df8
mahamudul5 <mh.bilash22@gmail.com>
2022-03-26 04:36:39 +0600

    distucuring

--- src/components/single-package/SinglePackage.js
+++ src/components/single-package/SinglePackage.js
@@ -3,14 +3,14 @@ import { GiSelfLove } from "react-icons/gi";
 
 const SinglePackage = (props) => {
     console.log(props.tourPackage.name);
-
+    const { image, name, price, id } = props.tourPackage;
     return (
         <div className='col-md-4'>
             <div className="card p-3 border">
-                <img className='img-fluid ' src={props.tourPackage.image} alt="" />
-                <h4>{props.tourPackage.name}</h4>
-                <h5>tourCost:{props.tourPackage.price}</h5>
-                <p>packageId:{props.tourPackage.id}</p>
+                <img className='img-fluid ' src={image} alt="" />
+                <h4>package Name:{name}</h4>
+                <h5>tour Cost: {price}</h5>
+                <p>package Id: {id}</p>
                 <button className='btn btn-primary'>Add to favourite <GiSelfLove /> </button>
             </div>
 
